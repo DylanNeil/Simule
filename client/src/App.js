43d0fa3425129b2dab8ed7ba700dfa0ac8f6ea56
Dylan Neil <dpagillo@gmail.com>
2020-09-24 00:56:09 -0400

    added materialUI Grid and materialUI container

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import Button from "./simpleButton";
 import theme from "./theme";
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import Eventcard from './eventcard.js';
+
 
 
 class App extends Component {
@@ -13,12 +16,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+       <Container maxWidth="lg">
         <header className="App-header">
           <h1 className="App-title">Event Finder</h1>
+          <Grid container spacing={3}>
+          <Grid item xs={12} sm={3}>
+          <Eventcard />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+          <Eventcard />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+          <Eventcard />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+          <Eventcard />
+          </Grid>
+          </Grid>          
         </header>
-        <MuiThemeProvider theme={theme}>
-        <Button>Button Sample - Styled from theme.js</Button>
-      </MuiThemeProvider>
+        </Container>
       </div>
     );
   }

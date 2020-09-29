df7bb6e014370767d5019b5d27c556066042b323
Dylan Pagillo <Dpagillo@gmail.com>
2020-09-29 19:05:42 -0400

    Added props to eventcard (#48)
    * Added working delete function to server/server.js
    
    * In eventcard.js - changed the size of transform: scale on hover. Replaced button component with simpleButton.js
    
    * changed padding size and hover: background-color in simpleButton.js
    
    * added materialUI Grid and materialUI container
    
    * replaced text and images in eventcard.js  with props passed down from the App.js

--- client/src/eventcard.js
+++ client/src/eventcard.js
@@ -5,16 +5,17 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Button from "./simpleButton";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
     textAlign: 'left',
     transition: '0.3s',
+    lineclamp: '3',
     '&:hover':{
-      transform: 'scale(1.02)',
+      transform: 'scale(1.005)',
       transition: '0.3s'
     }
   },
@@ -23,7 +24,9 @@ const useStyles = makeStyles({
   },
 });
 
-function EventCard() {
+
+
+function EventCard(props) {
   const classes = useStyles();
 
   return (
@@ -31,20 +34,22 @@ function EventCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="https://www.scitecheuropa.eu/wp-content/uploads/2019/07/ClaudioVentrella.jpg"
-          title="Contemplative Reptile"
+          image={props.image}
+          title="Futurama"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Good News Everyone! Futurama Trivia Night
+            {/* Good News Everyone! Futurama Trivia Night */}
+            {props.title}
           </Typography>
           <Typography variant="subtitle1" component="h4">
-           01/01/3021
+           {props.date}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew.
-            Anyone who laughs is a communist! Isn't it true that you have been paid for your testimony?
-          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {props.text}
+               {/* Alright, let's mafia things up a bit. Joey, burn down the ship. Clamps, burn down the crew.
+               Anyone who laughs is a communist! Isn't it true that you have been paid for your testimony? */}
+            </Typography> 
         </CardContent>
       </CardActionArea>
       <CardActions>
--- server/server.js
+++ server/server.js
@@ -4,35 +4,51 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const events = [
-
+  {id: 0,
+  name: 'Test Event'},
+  {id: 1,
+    name: 'Test Event'},
+  {id: 2,
+    name: 'Test Event'},
 ];
 
-let idCounter = 0;
+let idCounter = events.length -1;
 
 app.use(bodyParser.json());
 
-app.post('/events', function test2(req,res) {
+app.post('/events', function createEvent(req,res) {
   idCounter += 1;
   const eventObject = {
     id: idCounter,
     name: req.body.name
   };
   events.push(eventObject);
-  console.log(events);
   res.status(201).send({ eventObject });
 });
 
 app.delete('/events/:id',(req, res) => {
   const requestId = events.find((event) => event.id === req.params.id);
-  res.status(202).send(requestId);
-    return;
+  
+  for(let i=0; i<=events.length -1; i += 1){
+    const eventsID = events[i].id;
+    const currentID = req.params.id;
+    const index = events[i];
+       if(eventsID == currentID){
+         events.splice([i],1);
+       }
+       else{
+        res.status(404).send();        
+       }
+  }
+  res.status(204).send();
+  return;
 }); 
 
 // create a GET route
 app.get('/events/:id', (req, res) => {
-    const event = events.find((event) => event.id === req.params.id);
-    res.status(200).send(event);
-      return;
+  const event = events.find((event) => event.id === req.params.id);
+  res.status(200).send(event);
+  return;
 });
 
 
